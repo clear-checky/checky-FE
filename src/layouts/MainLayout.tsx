08861e8c84193fd36740c124a75389d6da0eb2be
wnhaoo <153691178+wnhaoo@users.noreply.github.com>
2025-09-09 06:13:51 +0900

    feat: 색상 테마 추가

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,7 +3,7 @@ import Header from '../components/common/Header';
 
 const MainLayout = () => {
   return (
-    <div className="min-h-screen bg-green-50 w-full">
+    <div className="min-h-screen bg-gradient-primary w-full">
       <Header />
       <main className="w-full">
         <Outlet />
